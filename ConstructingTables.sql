ed3d2c19aa3bfeeaf0ea8a973b271b476a1f83e8
sabahb3 <sabahbaara4@gmail.com>
2022-03-22 15:23:55 +0200

    Creating Episode Enemy table

--- ConstructingTables.sql
+++ ConstructingTables.sql
@@ -12,3 +12,12 @@ EnemyName VARCHAR(200),
 Description VARCHAR(500)
 CONSTRAINT pk_Enemy_Id PRIMARY KEY (EnemyId)
 );
+
+CREATE TABLE tblEpisodeEnemy(
+EpisodeEnemyId int,
+EpisodeId int,
+EnemyId int, 
+CONSTRAINT pk_Episode_Enemy_Id PRIMARY KEY (EpisodeEnemyId),
+CONSTRAINT FK_Episode_Enemy_Id FOREIGN KEY (EnemyId)
+REFERENCES tblEnemy(EnemyId)
+);
