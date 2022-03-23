e59cd0e175c805f2495896c26e6d7f601f04ffd2
sabahb3 <sabahbaara4@gmail.com>
2022-03-23 11:06:08 +0200

    adding records to the author table

---
+++ InsertingRecords.sql
@@ -0,0 +1,9 @@
+use DoctorWho;
+
+INSERT into tblAuthor VALUES ('Aisha Marmash'),
+                             ('Sara Sawafta'),
+                             ('Asad Jamal'),
+                             ('Rawan Ahmad'),
+                             ('Qamar Ashour');
+
+SELECT * FROM tblAuthor;
